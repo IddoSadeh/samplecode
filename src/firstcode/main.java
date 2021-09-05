15d093a5647af681ebc9e913f1dabac2a2e4ed8a
IddoSadeh <iddosadeh@gmail.com>
2021-09-04 20:09:20 -0700

    Second Commit!

--- src/firstcode/main.java
+++ src/firstcode/main.java
@@ -1,4 +1,35 @@
 package firstcode;
 
-public class main {
+class main {
+    public static void main(String[] args) {
+        int[] boo = {0, 1, 2, 3, 4};
+
+        int[] renamingcode = new int[100];
+        for(int i = 0; i < 100; i++){
+            renamingcode[i] = i;
+        }
+        System.out.println("The number of even numbers in boo is:");
+        System.out.println(countEvenNum(boo, 5));
+
+        System.out.println("The number of even numbers in renamingcode is:");
+        System.out.println(countEvenNum(renamingcode, 100));
+
+    }
+
+
+
+    private static int countEvenNum(int[] arr, int size){
+        int count = 0;
+
+        for(int i = 0; i < size; i++){
+            if((arr[i]) % 2 == 0){
+                count++;
+            }
+            else{
+
+            }
+        }
+
+        return count;
+    }
 }
